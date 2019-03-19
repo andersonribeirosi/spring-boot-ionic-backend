f538b8286ed095ef65fd4a9855e18e1690c5088b
Anderson Ribeiro <andersonribeiro.si@hotmail.com>
2019-03-19 14:52:47 -0300

    Inserindo uma nova Categoria através do POST

--- src/main/java/br/com/elfrarep/resources/CategoriaResource.java
+++ src/main/java/br/com/elfrarep/resources/CategoriaResource.java
@@ -1,12 +1,16 @@
 package br.com.elfrarep.resources;
 
+import java.net.URI;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import br.com.elfrarep.domain.Categoria;
 import br.com.elfrarep.service.CategoriaService;
@@ -14,33 +18,21 @@ import br.com.elfrarep.service.CategoriaService;
 @RestController
 @RequestMapping(value = "/categorias")
 public class CategoriaResource {
-	
+
 	@Autowired
 	public CategoriaService service;
-	
+
 	@CrossOrigin
-	@RequestMapping(value="/{id}", method = RequestMethod.GET)
-	public ResponseEntity<?> find(@PathVariable Integer id){
+	@RequestMapping(value = "/{id}", method = RequestMethod.GET)
+	public ResponseEntity<?> find(@PathVariable Integer id) {
 		Categoria obj = service.buscar(id);
 		return ResponseEntity.ok().body(obj);
 	}
-	
-	
-	//@RequestMapping(value="/{id}", method = RequestMethod.GET)
-	//public ResponseEntity<?> find(@PathVariable Integer id){
-		//Categoria obj = service.buscar(id);
-		//return ResponseEntity.ok().body(obj);
-	//}
-	
-	//@CrossOrigin
-	//@RequestMapping(value="/{id}", method = RequestMethod.GET)
-	//public ResponseEntity<?> find(@PathVariable Integer id){
-		//List<Categoria> list = service.buscar(id);
-		//List<CategoriaDTO> listDto = list.stream().
-		//return ResponseEntity.ok().body(obj);
-		//Categoria obj = service.buscar(id);
-		//return ResponseEntity.ok().body(obj);
-	
+
+	@RequestMapping(method = RequestMethod.POST)
+	public ResponseEntity<Void> insert(@RequestBody Categoria obj){
+		obj = service.insert(obj);
+		URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(obj.getId()).toUri();
+		return ResponseEntity.created(uri).build();
 	}
-
-
+}
