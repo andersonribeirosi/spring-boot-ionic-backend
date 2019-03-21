0fd20e277e95bef466ef97b40683b76ec6b5ccb2
Anderson Ribeiro <andersonribeiro.si@hotmail.com>
2019-03-21 14:33:29 -0300

    Paginação com parametros opcionais nas requisições

--- src/main/java/br/com/elfrarep/service/CategoriaService.java
+++ src/main/java/br/com/elfrarep/service/CategoriaService.java
@@ -4,6 +4,9 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Sort.Direction;
 import org.springframework.stereotype.Service;
 
 import br.com.elfrarep.domain.Categoria;
@@ -13,23 +16,23 @@ import br.com.elfrarep.service.exceptions.ObjectNotFoundException;
 
 @Service
 public class CategoriaService {
-	
+
 	@Autowired
 	private CategoriaRepository repo;
-	
+
 	public Categoria find(Integer id) {
 		Categoria obj = repo.findOne(id);
-		if(obj == null) {
+		if (obj == null) {
 			throw new ObjectNotFoundException("Objeto não encontrado");
 		}
 		return (obj);
 	}
-	
+
 	public Categoria insert(Categoria obj) {
 		obj.setId(null);
 		return repo.save(obj);
 	}
-	
+
 	public Categoria update(Categoria obj) {
 		find(obj.getId());
 		return repo.save(obj);
@@ -44,7 +47,12 @@ public class CategoriaService {
 		}
 	}
 
-	public List<Categoria> findAll(){
+	public List<Categoria> findAll() {
 		return repo.findAll();
+	}
+
+	public Page<Categoria> findPage(Integer page, Integer linesPerPage, String orderBy, String direction) {
+		PageRequest pageRequest = new PageRequest(page, linesPerPage, Direction.valueOf(direction), orderBy );
+		return repo.findAll(pageRequest);
 	}
 }
